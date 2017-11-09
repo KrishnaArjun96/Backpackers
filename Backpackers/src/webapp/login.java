f67ab2eea3376b7d50c453004cba92d8f6fe0463
Unknown <krishna.arjun96@gmail.com>
2017-11-08 23:21:56 -0500

    Final jdbc fix

--- Backpackers/src/webapp/login.java
+++ Backpackers/src/webapp/login.java
@@ -5,13 +5,29 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.Statement;
 
 @WebServlet(name = "login")
 public class login extends HttpServlet {
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, java.io.IOException {
 
-        request.setAttribute("username",request.getParameter("username"));
-        request.getRequestDispatcher("/welcome.jsp").forward(request,response);
+        request.setAttribute("username", request.getParameter("username"));
+        try {
+            Class.forName("com.mysql.jdbc.Driver");
+            Connection con = DriverManager.getConnection("jdbc:mysql://localhost:3306/backpackers", "root", "root");
+            Statement stmt = con.createStatement();
+            ResultSet rs = stmt.executeQuery("select * from Airline");
+            while (rs.next())
+                System.out.println(rs.getString(1) + "  " + rs.getString(2));
+            con.close();
+        } catch (Exception e) {
+            System.out.println(e);
+        }
+
+        request.getRequestDispatcher("/welcome.jsp").forward(request, response);
 
     }
 
