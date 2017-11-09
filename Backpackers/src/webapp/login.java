9e481be3323083f316948bd93ed53da4e2f869d9
Unknown <krishna.arjun96@gmail.com>
2017-11-08 23:12:16 -0500

    reverting
    Getting .idea files back

--- Backpackers/src/webapp/login.java
+++ Backpackers/src/webapp/login.java
@@ -5,13 +5,18 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.Statement;
 
 @WebServlet(name = "login")
 public class login extends HttpServlet {
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, java.io.IOException {
 
-        request.setAttribute("username",request.getParameter("username"));
-        request.getRequestDispatcher("/welcome.jsp").forward(request,response);
+        request.setAttribute("username", request.getParameter("username"));
+        request.getRequestDispatcher("/welcome.jsp").forward(request, response);
+
 
     }
 
