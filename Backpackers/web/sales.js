4509bcc224a281052ef00de1eef7c40f2efc3e5e
Krishna Arjun <krishna.arjun96@gmail.com>
2017-12-05 05:04:56 -0500

    Updated sales report table to have date column

--- Backpackers/web/sales.js
+++ Backpackers/web/sales.js
@@ -20,7 +20,7 @@ $(function () {
             success: function (data) {
                 $(document).ready(function () {
                     $("#salesTable").find("tr:gt(0)").remove();
-                    for (var i = 0; i < data.length ; i++) {
+                    for (var i = 0; i < data.length; i++) {
                         $('#salesTable tbody').append('<tr>\n' +
                             '        <td class="center aligned">' + data[i].resr + '</td>\n' +
                             '        <td class="center aligned">' + data[i].rep + '</td>\n' +
